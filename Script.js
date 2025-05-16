9ac750c70be0c80c8f74e5d1ec885c45b76d6186
Amr Ragab <amrragab530@gmail.com>
2025-05-16 03:14:35 +0300

    The File

---
+++ Script.js
@@ -0,0 +1,47 @@
+const inputTask = document.getElementById("input-task");
+const taskList = document.getElementById("task-list");
+const btn = document.querySelector(".btn");
+
+function addTask () {
+    let taskValue = inputTask.value;
+    if(!taskValue) {
+        alert("You must a value");
+    } else {
+        let li = document.createElement("li");
+        let span = document.createElement("span");
+
+        li.innerHTML = taskValue;
+        taskList.appendChild(li);
+
+        span.innerHTML = "&times;";
+        li.appendChild(span);
+    }
+    inputTask.value = "";
+    setTask();
+}
+
+btn.addEventListener("click", addTask);
+taskList.addEventListener("click", function (e) {
+    if(e.target.tagName === "LI"){
+         e.target.classList.toggle("checked");
+         setTask();
+    }else if (e.target.tagName === "SPAN") {
+e. target.parentElement.remove();
+setTask();
+    }
+       
+});
+
+inputTask.addEventListener("keydown" , function (e) {
+    if (e.key === "Enter") {
+        addTask();
+    }
+}) 
+
+function setTask() {
+localStorage.setItem ("lists", taskList.innerHTML);
+}
+function loadTasks (){
+    taskList .innerHTML = localStorage.getItem("lists")
+}
+loadTasks();
